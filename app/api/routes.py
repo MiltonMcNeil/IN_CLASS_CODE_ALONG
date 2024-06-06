95c3438e001637742fa91cf9d31de4b623ee2279
JBQ1 JBQ1 <quizkid91102@gmail.com>
2024-06-06 17:40:02 -0400

    changes

--- app/api/routes.py
+++ app/api/routes.py
@@ -40,31 +40,36 @@ def get_contact(current_user_token):
 def get_Whisky_two(Whisky_user_token, id):
     fan = Whisky_user_token.token
     if fan == Whisky_user_token.token:
-        Whisky = Whisky.query.get(id)
-        response = Whisky_schema.dump(Whisky)
+        whisky = Whisky.query.get(id)
+        response = Whisky_schema.dump(whisky)
         return jsonify(response)
     else:
         return jsonify({"message": "Valid Token Required"}),401
 
-@api.route('/Whisky/<id>', methods = ['POST','PUT'])
+@api.route('/Whiskys/<id>', methods = ['PUT'])
 @token_required
 def update_Whisky(current_user_token,id):
-    Whisky = Whisky.query.get(id) 
-    Whisky.brand = request.json['brand']
-    Whisky.proof = request.json['proof']
-    Whisky.aged = request.json['aged']
-    Whisky.grain = request.json['grain']
-    Whisky.user_token = current_user_token.token
+    print(id)
+    print(current_user_token)
+    whisky = Whisky.query.get(id) 
+    print(whisky)
+    whisky.brand = request.json['brand']
+    whisky.proof = request.json['proof']
+    whisky.aged = request.json['aged']
+    whisky.grain = request.json['grain']
+    whisky.user_token = current_user_token.token
 
     db.session.commit()
-    response = Whisky_schema.dump(Whisky)
+    response = Whisky_schema.dump(whisky)
     return jsonify(response)
 
 @api.route('/Whiskys/<id>', methods = ['DELETE'])
 @token_required
 def delete_Whisky(current_user_token, id):
-    Whisky = Whisky.query.get(id)
-    db.session.delete(Whisky)
+    print(id)
+    whisky = Whisky.query.get(id)
+    db.session.delete(whisky)
     db.session.commit()
-    response = Whisky_schema.dump(Whisky)
+    response = Whisky_schema.dump(whisky)
     return jsonify(response)
+   
